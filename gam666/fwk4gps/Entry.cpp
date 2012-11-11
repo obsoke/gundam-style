ef47fba826713f35fe75c3a1da24443c48e25219
Dale Karp <me@dale.io>
2012-11-10 21:03:17 -0500

    Putting back original Entry.cpp

--- gam666/fwk4gps/Entry.cpp
+++ gam666/fwk4gps/Entry.cpp
@@ -9,35 +9,14 @@
 */
 
 #define WIN32_LEAN_AND_MEAN
-#include <iostream>
-#include <vector> 
 #include <windows.h> // for WinMain and Windows Types
-#include "Game\Utilities\ObjImporter.h"
-#include "APIVertex.h"    // for ...Vertex class definitions
-#include "Game\Utils.h"
-#include "Game\Face.h"
+
 #include "Game\Game.h"  // for the Design class definition
 
 // Entry point for the Application
 //
 int WINAPI WinMain(HINSTANCE hinst, HINSTANCE hprev, LPSTR cp, int show) {
-	std::vector<Vector> VertexList;
-	std::vector<Vector> VertexNormalsList;
-	std::vector<Vector> VertexTextureList;
-	std::vector<Face> FaceList;
-	ObjImporter::ImportObjModel("..\\..\\resources\\assets\\gundam.obj", &VertexList, &VertexNormalsList, &VertexTextureList, &FaceList);
-	debug("VertexList size: ");
-	debug(VertexList.size());
-	debug("\n");
-		debug("VertexNormalsList size: ");
-	debug(VertexNormalsList.size());
-	debug("\n");
-		debug("VertexTextureList size: ");
-	debug(VertexTextureList.size());
-	debug("\n");
-		debug("FaceList size: ");
-	debug(FaceList.size());
-	debug("\n");
-  //Game game(hinst, show);
-  return 0;
+
+  Game game(hinst, show);
+
 }
