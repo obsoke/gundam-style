d897d327754e6fef95d6db69a8486d486ab6b9f7
JSilver99 <jasilver1@learn.senecac.on.ca>
2012-11-13 20:54:45 -0500

    Fixing spacing issue in Mesh.h.

--- gam666/fwk4gps/Game/Mesh.h
+++ gam666/fwk4gps/Game/Mesh.h
@@ -8,22 +8,22 @@
 #include <vector>
 
 class Mesh {
-    void addTri(VertexList<Vertex>* vertexList, const Face& face);
-    void addQuad(VertexList<Vertex>* vertexList, const Face& face);
-    void addFace(VertexList<Vertex>* vertexList, const Face& face);
-    unsigned numberOfTriangles();
-    void normalizeSize(float scale = 1);
-  public:
-    float buildScale;
-    std::vector<Vector> vertices;
-    std::vector<Vector> uvs;
-    std::vector<Vector> normals;
-    std::vector<Face> faces;
+  void addTri(VertexList<Vertex>* vertexList, const Face& face);
+  void addQuad(VertexList<Vertex>* vertexList, const Face& face);
+  void addFace(VertexList<Vertex>* vertexList, const Face& face);
+  unsigned numberOfTriangles();
+  void normalizeSize(float scale = 1);
+public:
+  float buildScale;
+  std::vector<Vector> vertices;
+  std::vector<Vector> uvs;
+  std::vector<Vector> normals;
+  std::vector<Face> faces;
 
-    Mesh (float buildScale = 45) : buildScale(buildScale) { };
+  Mesh (float buildScale = 45) : buildScale(buildScale) { };
 
-    VertexList<Vertex>* build(bool normalize = true);
-    AABB calcAABB();
+  VertexList<Vertex>* build(bool normalize = true);
+  AABB calcAABB();
 };
 
 #endif
